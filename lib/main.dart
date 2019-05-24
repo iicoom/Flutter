6c638511bda7d4a3676f930512a8676b27e6dd4b
MXJ <maoxiaojie@fnxy.net.cn>
2019-05-24 18:35:09 +0800

    flutter_bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:startup_name/modal_class/function.dart';
 import 'package:startup_name/modal_class/genres.dart';
+import 'package:startup_name/bloc/change_theme_bloc.dart';
+import 'package:startup_name/bloc/change_theme_state.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,7 +14,7 @@ class MyApp extends StatelessWidget {
         title: 'Matinee',
         theme: ThemeData(
           primarySwatch: Colors.blue, canvasColor: Colors.transparent),
-        home: Text('dfasfasd'),
+        home: Text('欢迎来到首页...'),
     );
   }
 }
@@ -56,101 +59,4 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-}
-
-class RandomWordsState extends State<RandomWords> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Startup Name Generator'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search),
-            tooltip: 'Search',
-            onPressed: null,
-          ),
-        ],
-      ),
-      body: _buildSuggestions(),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text('Home'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            title: Text('Business'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            title: Text('School'),
-          ),
-        ],
-        currentIndex: 0,
-//        selectedItemColor: Colors.amber[800],
-        onTap: null,
-      ),
-      floatingActionButton: FloatingActionButton(
-        tooltip: 'Add', // used by assistive technologies
-        child: Icon(Icons.add),
-        onPressed: null,
-      ),
-    );
-  }
-
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: /*1*/ (context, i) {
-          if (i.isOdd) return Divider(); /*2*/
-
-          final index = i ~/ 2; /*3*/
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  Widget _buildRow(WordPair pair) {
-    return ListTile(
-      title: Text(
-        pair.asCamelCase,
-        style: _biggerFont,
-      ),
-    );
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  RandomWordsState createState() => RandomWordsState();
-}
-
-class MyButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        print('MyButton was tapped!');
-      },
-      child: Container(
-        height: 36.0,
-        padding: const EdgeInsets.all(8.0),
-        margin: const EdgeInsets.symmetric(horizontal: 8.0),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(5.0),
-          color: Colors.lightGreen[500],
-        ),
-        child: Center(
-          child: Text('Engage'),
-        ),
-      ),
-    );
-  }
 }
--- lib/modal_class/function.dart
+++ lib/modal_class/function.dart
@@ -1,8 +1,8 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:startup_name/api/endpoints.dart';
-import 'package:movies/modal_class/credits.dart';
-import 'package:movies/modal_class/genres.dart';
+import 'package:startup_name/modal_class/credits.dart';
+import 'package:startup_name/modal_class/genres.dart';
 import 'package:startup_name/modal_class/movie.dart';
 
 Future<List<Movie>> fetchMovies(String api) async {
@@ -11,4 +11,20 @@ Future<List<Movie>> fetchMovies(String api) async {
   var decodeRes = jsonDecode(res.body);
   movieList = MovieList.fromJson(decodeRes);
   return movieList.movies;
+}
+
+Future<Credits> fetchCredits(String api) async {
+  Credits credits;
+  var res = await http.get(api);
+  var decodeRes = jsonDecode(res.body);
+  credits = Credits.fromJson(decodeRes);
+  return credits;
+}
+
+Future<GenresList> fetchGenres() async {
+  GenresList genresList;
+  var res = await http.get(Endpoints.genresUrl());
+  var decodeRes = jsonDecode(res.body);
+  genresList = GenresList.fromJson(decodeRes);
+  return genresList;
 }
